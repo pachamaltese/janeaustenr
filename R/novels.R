ef01949cc550f82ddef6022d1dc01a0a6a80b354
ironholds <ironholds@gmail.com>
2016-04-08 16:37:45 -0400

    Doc tweaks

--- R/novels.R
+++ R/novels.R
@@ -1,3 +1,11 @@
+#' An R Package for Jane Austen's Complete Novels
+#' This package contains the complete text of Jane Austen's novels,
+#' formatted to be convenient for text analysis.
+#' @docType package
+#' @name janeaustenr
+#' @aliases janeaustenr janeaustenr-package
+NULL
+
 #' The text of Jane Austen's novel "Sense and Sensibility"
 #'
 #' A dataset containing the text of Jane Austen's 1811 novel "Sense and 
